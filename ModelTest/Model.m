2d26135990c88610eaeea011b4599bf82b6a0b9f
KiWoongJin <jkw87first@naver.com>
2014-01-09 11:01:03 +0900

    ModelTest
    1.1.1실습입니다.

---
+++ ModelTest/Model.m
@@ -0,0 +1,48 @@
+//
+//  Model.m
+//  ModelTest
+//
+//  Created by SDT-1 on 2014. 1. 9..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import "Model.h"
+
+@implementation Model
+
+static Model *instance;
+
++(Model *)sharedModel{
+    if(nil == instance)
+        instance = [[Model alloc]init];
+    return instance;
+}
+
+- (id)init{
+    self = [super init];
+    if(self){
+        self.data = [[NSMutableArray alloc]initWithObjects:@"iPhone",@"iPod",@"MacBook Air",@"MacBook Pro",@"MacBook Pro Retian", @"iMac", @"MacPro", nil];
+    }
+    return self;
+}
+
+- (id)productAt:(int)productId{
+    return [self.data objectAtIndex:productId];
+}
+
+- (int)numberOfProducts{
+    return [self.data count];
+}
+
+- (void)addProduct:(id)product{
+    [self.data addObject:product];
+}
+
+- (void)removeProductAt:(int)index{
+    [self.data removeObjectAtIndex:index];
+}
+
+
+
+
+@end
